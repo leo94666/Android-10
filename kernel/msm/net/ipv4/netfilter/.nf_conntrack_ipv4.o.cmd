f5a7eb6c236d9bce689da41173c058aecb4c9d51
Leo <719947897@qq.com>
2021-12-01 10:08:56 +0800

    First Commit

---
+++ kernel/msm/net/ipv4/netfilter/.nf_conntrack_ipv4.o.cmd
@@ -0,0 +1,1 @@
+cmd_net/ipv4/netfilter/nf_conntrack_ipv4.o := /opt/ndk/android-ndk-r17c/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ld -EL    -r -o net/ipv4/netfilter/nf_conntrack_ipv4.o net/ipv4/netfilter/nf_conntrack_l3proto_ipv4_compat.o net/ipv4/netfilter/nf_conntrack_l3proto_ipv4.o net/ipv4/netfilter/nf_conntrack_proto_icmp.o 
